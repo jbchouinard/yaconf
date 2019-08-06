39a7be513f3162b51b96a4f9ae3a1d394adb3a02
Jerome Boisvert-Chouinard <github@jbchouinard.net>
2019-08-05 22:15:32 -0400

    Add more option types

--- testclick.py
+++ testclick.py
@@ -3,7 +3,12 @@ from pprint import pprint
 import click
 import yamconfig.click
 from yamconfig import Config, Option
-from yamconfig.types import List
+from yamconfig.types import Choice, List, LogLevel, Optional
+
+
+class LoggingConfig(Config):
+    level = Option("log level", type=LogLevel(), default="INFO")
+    mode = Option("log mode", type=Choice("file", "syslog"), default="file")
 
 
 class SubConfig(Config):
@@ -14,9 +19,11 @@ class SubConfig(Config):
 class AppConfig(Config):
     x = Option("x", type=int, default=0)
     y = Option("y", type=int, default=0)
+    ts = Option("ts", type=List(int), default=[])
+    o = Option("o", type=Optional(int), default=None)
     s1 = Option("s1", type=SubConfig)
     s2 = Option("s2", type=SubConfig)
-    ts = Option("ts", type=List(int), default=[])
+    logging = Option("logging", type=LoggingConfig)
 
 
 @click.command()
--- yamconfig/types.py
+++ yamconfig/types.py
@@ -1,8 +1,53 @@
+import logging
+
+
 class List:
     def __init__(self, type):
         self.type = type
 
+    def __repr__(self):
+        return "List({!r})".format(self.type)
+
     def __call__(self, value):
         if isinstance(value, str):
-            value = value.split(',')
+            value = value.split(",")
         return [self.type(v) for v in value]
+
+
+class Optional:
+    def __init__(self, type):
+        self.type = type
+
+    def __repr__(self):
+        return "Optional({!r})".format(self.type)
+
+    def __call__(self, value):
+        if value is None:
+            return None
+        return self.type(value)
+
+
+class Choice:
+    def __init__(self, *choices):
+        self.choices = set(choices)
+
+    def __repr__(self):
+        return "Choice({!r})".format(", ".join(self.choices))
+
+    def __call__(self, value):
+        if value not in self.choices:
+            raise ValueError(value)
+        return value
+
+
+class LogLevel:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "LogLevel()"
+
+    def __call__(self, value):
+        if isinstance(value, str):
+            value = logging.getLevelName(value)
+        return int(value)
